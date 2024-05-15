0248be52a0bf9024ed8c96e06b2d0d3509c35438
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2024-05-15 22:50:35 +0200

    Added Selenium tests, finished logic for G

--- src/main/java/se/reky/hakan/web/PlayerController.java
+++ src/main/java/se/reky/hakan/web/PlayerController.java
@@ -3,8 +3,6 @@ package se.reky.hakan.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import se.reky.hakan.model.Player;
 import se.reky.hakan.service.PlayerService;
 
 @Controller
@@ -20,4 +18,6 @@ public class PlayerController {
         model.addAttribute("players", playerService.findAllPlayersSortedByExperience());
         return "players"; // Name of the Thymeleaf template
     }
+    
+
 }
---
+++ src/test/java/se/reky/hakan/web/PlayerControllerTest.java
@@ -0,0 +1,85 @@
+package se.reky.hakan.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled
+class PlayerControllerTest {
+    
+    // TODO: 2024-05-15 Ändra namn på spelaer
+/*
+* Spela alltid två rundor!!
+* */
+    
+    private WebDriver driver;
+    private String playersUrl = "http://localhost:8080/players";
+    
+    
+    @BeforeAll
+//    sätt upp webdriver manager
+    public static void init(){
+        WebDriverManager.chromedriver().setup();
+    
+    }
+    
+//    instansiera webdriver före varje test
+    @BeforeEach
+    void setDriver(){
+        driver = new ChromeDriver();
+    }
+    
+//    metod som stänger WebDriver efter varje test
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+//
+//1) Verifiera att rätt antal players visas i listan (exempel: har du spelat två spel bör det finnas
+//            två players i listan och detta ska verifieras)
+//2) Verifiera att namnet på den första playern i listan visas på sidan (använd metoden
+//  isDisplayed() i klassen WebElement).
+//3) Testa att sidans titel överensstämmer med förväntat värde
+//4) Testa att knappen har texten = Logga in
+    
+    @Test
+    void numOfPlayersShouldBeTwo() {
+        driver.get(playersUrl);
+        List<WebElement> players = driver.findElements(By.tagName("li"));
+        assertEquals(2, players.size());
+    }
+    
+    @Test
+    void firstNameOfPlayerListShouldBeVisible(){
+        driver.get(playersUrl);
+        List<WebElement> players = driver.findElements(By.className("player-name"));
+        boolean playerIsDisplayed = players.get(0).isDisplayed();
+        assertTrue(playerIsDisplayed);
+    }
+    
+    @Test
+    void pageShouldHaveCorrectTitle(){
+        driver.get(playersUrl);
+        String result = driver.getTitle();
+        assertEquals("Players List", result);
+    }
+    
+    @Test
+    void buttonShouldHaveTheRightText(){
+        driver.get(playersUrl);
+        String result = driver.findElement(By.tagName("button")).getText();
+        assertEquals("Logga in", result);
+    }
+    
+    
+    
+}
